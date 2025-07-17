47dff924d2885ba56aa7a5a7be37ed7e71728d8f
Gaurik-28 <iamgaurikhiste@gmail.com>
2025-07-17 19:03:08 +0530

    Add files via upload

---
+++ assignment 7/5.py
@@ -0,0 +1,15 @@
+
+for i in range(1,6):
+    x=i
+    for j in range(1,7-i):
+        if(i==1 or j==1 or j==6-i):
+            print(x,end= " ")
+        else:
+            print(" ",end=" ")
+        x=x+1    
+    print()      
+# 1 2 3 4 5 
+# 2     5
+# 3   5
+# 4 5
+# 5          
